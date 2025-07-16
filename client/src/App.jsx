e9e434a255b1a002a358f883ee5e92eabd83bbdc
sqlv <agvstin.77@gmail.com>
2025-07-15 23:12:52 -0300

    Empeze a hacer el FronEnd, implementacion del primer componente NavBar

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+import Navbar from './NavBar/Navbar'
+
+function App() {
+
+  return (
+    <div className='app-container'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/NavBar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.css";
+import logo from '../assets/images/logo.png';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img className="navbar-logo" src={logo} alt="Aplication Logo"/>
+      <div className="navbar-links">
+        <Link to="/">Inicio</Link>
+        <Link to="/about">Sobre nosotros</Link>
+        <Link to="/contact">Contactanos</Link>
+        <Link to="/frequentQuest">Preguntas Frecuentes</Link>
+        <Link to="/planes">Planes</Link>
+        <div className="navbar-auth-links">
+          <Link to="/login"><button className="btn">Iniciar Sesion</button></Link>
+          <Link to="/register"><button className="btn-secondary">Registrarse</button></Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
