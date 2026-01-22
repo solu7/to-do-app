97c9d1b0e069c92bfaff4aecd53086dd0467496e
sqlv <agvstin.77@gmail.com>
2026-01-22 20:07:52 -0300

    fix: arreglo de error al descargar cv Fichero no existente

--- client/src/core/constants/assets.js
+++ client/src/core/constants/assets.js
@@ -1,2 +1,2 @@
-export const CV_URL = "../../../public/assets/docs/Agustin-Salva-Dev-CV.pdf";
+export const CV_URL = "/Agustin-Salva-Dev-CV.pdf";
 export const CV_FILENAME = "Agustin-Salva-Dev-CV.pdf";
