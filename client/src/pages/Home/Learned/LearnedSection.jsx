3f1efeff39b06b70e1c15fafb6628a5fa0a5afc1
sqlv <agvstin.77@gmail.com>
2026-01-22 20:02:14 -0300

    Cambio de la informacion en la seccion Learned

--- client/src/pages/Home/Learned/LearnedSection.jsx
+++ client/src/pages/Home/Learned/LearnedSection.jsx
@@ -8,18 +8,47 @@ function LearnedSection() {
       </h2>
       <div className="learned__list">
         <ul>
-          <li>Diseñar e implementar una API RESTful</li>
-          <li>Autenticar seguramente un usuario</li>
-          <li>Diseñar de una base de datos relacional escalable</li>
+          <li>Manejar estados globales con Context API</li>
           <li>
-            Armar una buena arquitectura de carpetas y estructura de codigo
+            Cómo funcionan los tokens JWT y por qué usar cookies para proteger
+            la información del usuario.
+          </li>
+          <li>
+            Crear middlewares para crear filtros de seguridad automáticos antes
+            de procesar la solicitud.
+          </li>
+          <li>
+            Como implementar esquemas de validación para asegurar que los datos
+            siempre lleguen como se necesiten.
+          </li>
+          <li>
+            Que tener en cuenta al diseñar una base de datos relacional para
+            conectar la información de forma lógica.
           </li>
         </ul>
         <ul>
-          <li>Como documentar una API</li>
-          <li>Implementar seguridad y validación de datos</li>
-          <li>Desplegar mis proyectos en la web</li>
-          <li>Me familiarize mas con el testing e2e</li>
+          <li>
+            Organizar y separar el código, en esta app hice un buen trabajo para
+            que el proyecto no se me fuera de las manos y fuera mas fácil de
+            mantener.
+          </li>
+          <li>
+            Darle mas importancia a la experiencia del usuario, esto trate
+            mejorarlo con colores, modales de aviso, animaciones, etc.
+          </li>
+          <li>
+            Todo el proceso de llevar un desarrollo local a un entorno de
+            producción real, configurando servidores y bases de datos en la
+            nube.
+          </li>
+          <li>
+            Me permitio familiarizarme mas con el testing que era un sector
+            desconocido para mi.
+          </li>
+          <li>
+            Manejar consultas SQL mas robustas y complicadas, para poder hacer
+            peticiones mas eficientes
+          </li>
         </ul>
       </div>
     </div>
